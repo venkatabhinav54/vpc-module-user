d76f8fa8c38a2684744bfafed0a41d239fdacf46
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-09-26 00:34:48 +0530

    user profile

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+    project_name = var.project_name
+    vpc_cidr = var.vpc_cidr
+    environment = var.environment
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+}
